d82605ee9d95e1b0ded686e47ff5522f4da0c3a0
Muhammad Talha <talhashabbir9@gmail.com>
2021-11-17 19:50:57 +0500

    added rating

--- src/logged_in/components/sellingZone/workerProfile/WorkerProfile.js
+++ src/logged_in/components/sellingZone/workerProfile/WorkerProfile.js
@@ -6,6 +6,9 @@ import { Button } from "@material-ui/core";
 import classNames from "classnames";
 import useWorkerProfile from "../../../../hooks/workerProfile/useWorkerProfile";
 import { useHistory } from "react-router-dom";
+import { Rating } from "@material-ui/lab";
+import { reviewTypesStrings } from "../../../../config/enums/reviewTypes";
+import BoxCircularProgress from "../../../../shared/components/BoxCircularProgress";
 
 const styles = (theme) => ({
   card: {
@@ -26,27 +29,56 @@ const WorkerProfile = ({ classes }) => {
     >
       <Grid container spacing={3} justifyContent="center" alignItems="center">
         {isLoading ? (
-          <span>Loading...</span>
+          <BoxCircularProgress />
         ) : isError ? (
           <span>Error Occurred</span>
         ) : (
-          <Grid item xs={8}>
-            <Card className={classes.card}>
-              <Button
-                onClick={() => {
-                  history.push("/user/create_worker_profile");
-                }}
-              >
-                {workerProfile ? "Edit Profile" : "Create Profile"}
-              </Button>
-              <Typography variant="h6">{workerProfile?.description}</Typography>
-              <Typography variant="body2">
-                {workerProfile?.skills.map((skill) => {
-                  return skill.name + ", ";
-                })}
-              </Typography>
-            </Card>
-          </Grid>
+          <>
+            <Grid item xs={8}>
+              <Card className={classes.card}>
+                <Button
+                  onClick={() => {
+                    history.push("/user/create_worker_profile");
+                  }}
+                >
+                  {workerProfile ? "Edit Profile" : "Create Profile"}
+                </Button>
+                <Typography variant="h6">
+                  {workerProfile?.description}
+                </Typography>
+                <Typography variant="body2">
+                  {workerProfile?.skills.map((skill) => {
+                    return skill.name + ", ";
+                  })}
+                </Typography>
+                <Typography variant="h6">Overall Rating</Typography>
+
+                <Rating
+                  value={workerProfile?.rating}
+                  precision={0.5}
+                  disabled={true}
+                />
+              </Card>
+            </Grid>
+            <Grid item xs={8}>
+              <Typography variant="h2">Reviews</Typography>
+            </Grid>
+
+            {workerProfile?.reviews.map((review) => (
+              <Grid item xs={8}>
+                <Card className={classes.card}>
+                  <Typography variant="body2">
+                    Given By: {review?.given_by?.name}
+                  </Typography>
+                  <Typography variant="body2">{review?.review_text}</Typography>
+                  <Typography variant="body2">
+                    Review Type: {reviewTypesStrings[review?.review_type]}
+                  </Typography>
+                  <Rating value={review.rating} disabled={true} />
+                </Card>
+              </Grid>
+            ))}
+          </>
         )}
         {/* {openBidDialog && (
           <CreateBid
