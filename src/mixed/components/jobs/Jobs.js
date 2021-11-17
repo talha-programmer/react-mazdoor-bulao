41c8b93c7fe0377244bd6c7ce6596fe56258be9a
Muhammad Talha <talhashabbir9@gmail.com>
2021-11-17 19:49:27 +0500

    applied styles for job component

--- src/mixed/components/jobs/Jobs.js
+++ src/mixed/components/jobs/Jobs.js
@@ -14,6 +14,11 @@ import format from "date-fns/format";
 import { useHistory } from "react-router-dom";
 import { useQueryClient } from "react-query";
 import queryKeys from "../../../config/queryKeys";
+import { subDays, formatDistance } from "date-fns";
+import { LocationOn } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles";
+import shortenString from "../../../shared/functions/shortenString";
+import useGlobalClasses from "../../../hooks/style/useGlobalClasses";
 
 const styles = (theme) => ({
   // blogContentWrapper: {
@@ -31,11 +36,23 @@ const styles = (theme) => ({
   // },
   // noDecoration: {
   //   textDecoration: "none !important"
-  // }import useAppliedJobs from '../../../hooks/user/useAppliedJobs';
+  // }
 
   card: {
     boxShadow: theme.shadows[2],
-    padding: 20
+    padding: 20,
+
+    "&:hover": {
+      backgroundColor: theme.colors.background,
+      cursor: "pointer",
+      color: theme.colors.darkBlack
+    }
+  },
+  categoryName: {
+    borderRadius: 50,
+    padding: "4px 10px 4px 10px",
+    backgroundColor: "#f2f2f2",
+    marginRight: 10
   }
 });
 
@@ -43,12 +60,18 @@ function Jobs(props) {
   const { classes, selectJobs } = props;
   const history = useHistory();
   const queryClient = useQueryClient();
+  const currentDate = new Date();
+  const globalClasses = useGlobalClasses();
 
   // We have fetched jobs already in Routing. That's why accessing
   // them with queryClient
   let jobs = queryClient.getQueryData(queryKeys.jobs);
 
   useEffect(selectJobs, [selectJobs]);
+
+  const openJobDetails = (job) => {
+    history.push(`/jobs/${job.url}`);
+  };
 
   return (
     <Box
@@ -62,29 +85,40 @@ function Jobs(props) {
         ) : (
           jobs.map((job) => (
             <Grid item xs={8}>
-              <Card className={classes.card}>
-                <Typography variant="h5">{job.title}</Typography>
-                <Typography variant="body2">
-                  {format(new Date(job.created_at), "PPP", {
-                    awareOfUnicodeTokens: true
-                  })}
+              <Card
+                className={classes.card}
+                onClick={() => openJobDetails(job)}
+              >
+                <Typography variant="h5" className={globalClasses.mb_10}>
+                  {job.title}
                 </Typography>
-                <Typography variant="body2">
-                  Location: {job.location}
+                <Typography variant="subtitle1" className={globalClasses.mb_10}>
+                  Est. Budget: RS {job.budget} - Posted:{" "}
+                  {formatDistance(
+                    subDays(new Date(job.created_at), 0),
+                    currentDate,
+                    { addSuffix: true }
+                  )}
+                  {" - Est. time: "} {job.deadline} days
                 </Typography>
-                <Typography variant="body2">Budget: RS {job.budget}</Typography>
-                <Typography variant="body2">
-                  Deadline {job.deadline} days
-                </Typography>
-                <Typography variant="body2">{job.details}</Typography>
 
-                <Button
-                  onClick={() => {
-                    history.push(`/jobs/${job.url}`);
-                  }}
-                >
-                  Job Details
-                </Button>
+                <Typography variant="body1" className={globalClasses.mb_5}>
+                  {shortenString(job.details)}
+                </Typography>
+
+                <Typography variant="body1" className={globalClasses.mb_10}>
+                  {job?.categories?.map((category) => (
+                    <span className={classes.categoryName}>
+                      {category.name}
+                    </span>
+                  ))}
+                </Typography>
+
+                <Typography variant="body2">
+                  Posted By: {job.posted_by.name}{" "}
+                  <LocationOn style={{ fontSize: 18 }} />
+                  {job.location}
+                </Typography>
               </Card>
             </Grid>
           ))
--- src/mixed/components/singleJob/SingleJob.js
+++ src/mixed/components/singleJob/SingleJob.js
@@ -1,6 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import useJob from "../../../hooks/jobs/useJob";
-import { Box, Card, withStyles, withWidth } from "@material-ui/core";
+import {
+  Avatar,
+  Box,
+  Card,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  withStyles,
+  withWidth
+} from "@material-ui/core";
 import { Grid } from "@material-ui/core";
 import { Typography } from "@material-ui/core";
 import format from "date-fns/format";
@@ -8,11 +18,26 @@ import { Button } from "@material-ui/core";
 import CreateBid from "../../../logged_in/components/sellingZone/createBid/CreateBid";
 import useAppliedJobs from "../../../hooks/user/useAppliedJobs";
 import classNames from "classnames";
+import "react-image-gallery/styles/css/image-gallery.css";
+import ReactImageGallery from "react-image-gallery";
+import BoxCircularProgress from "../../../shared/components/BoxCircularProgress";
+import smoothScrollTop from "../../../shared/functions/smoothScrollTop";
+import { LocationOn, Phone, PhoneAndroidRounded } from "@material-ui/icons";
+import { formatDistance, subDays } from "date-fns";
+import useGlobalClasses from "../../../hooks/style/useGlobalClasses";
+import { Rating } from "@material-ui/lab";
 
 const styles = (theme) => ({
   card: {
     boxShadow: theme.shadows[2],
     padding: 20
+  },
+
+  categoryName: {
+    borderRadius: 50,
+    padding: "4px 10px 4px 10px",
+    backgroundColor: "#f2f2f2",
+    marginRight: 10
   }
 });
 
@@ -20,6 +45,20 @@ const SingleJob = ({ jobId, classes }) => {
   const { data: job, isLoading, isError } = useJob(jobId);
   const { data: appliedJobs } = useAppliedJobs();
   const [openBidDialog, setOpenBidDialog] = useState(false);
+  const globalClasses = useGlobalClasses();
+
+  let images = [];
+
+  job?.images.forEach((image) => {
+    const singleImage = {
+      original: image.image_url,
+      thumbnail: image.image_thumbnail_url
+    };
+    images.push(singleImage);
+  });
+
+  useEffect(smoothScrollTop, [smoothScrollTop]);
+
   return (
     <Box
       display="flex"
@@ -28,36 +67,128 @@ const SingleJob = ({ jobId, classes }) => {
     >
       <Grid container spacing={3} justifyContent="center" alignItems="center">
         {isLoading ? (
-          <span>Loading...</span>
+          <BoxCircularProgress />
         ) : isError ? (
           <span>Error Occurred</span>
         ) : (
-          <Grid item xs={8}>
-            <Card className={classes.card}>
-              <Typography variant="h5">{job.title}</Typography>
-              <Typography variant="body2">
-                {format(new Date(job.created_at), "PPP", {
-                  awareOfUnicodeTokens: true
-                })}
-              </Typography>
-              <Typography variant="body2">Location: {job.location}</Typography>
-              <Typography variant="body2">Budget: RS {job.budget}</Typography>
-              <Typography variant="body2">
-                Deadline {job.deadline} days
-              </Typography>
-              <Typography variant="body2">{job.details}</Typography>
-
-              {appliedJobs && !(job.id in appliedJobs) && (
-                <Button
-                  onClick={() => {
-                    setOpenBidDialog(true);
-                  }}
-                >
-                  Bid on this job
-                </Button>
+          <>
+            <Grid item xs={8}>
+              {images.length > 0 && (
+                <>
+                  <ReactImageGallery items={images} />
+                  <br />
+                </>
               )}
-            </Card>
-          </Grid>
+              <Card className={classes.card}>
+                <Grid
+                  container
+                  direction="row"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Grid item>
+                    <Typography variant="h5" className={globalClasses.mb_10}>
+                      {job.title}
+                    </Typography>
+                  </Grid>
+
+                  <Grid item>
+                    {appliedJobs && !(job.id in appliedJobs) && (
+                      <Button
+                        variant="contained"
+                        color="secondary"
+                        size="large"
+                        onClick={() => {
+                          setOpenBidDialog(true);
+                        }}
+                      >
+                        Bid on this job
+                      </Button>
+                    )}
+                  </Grid>
+                </Grid>
+
+                <Typography variant="subtitle1" className={globalClasses.mb_10}>
+                  Est. Budget: RS {job.budget} - Posted:{" "}
+                  {formatDistance(
+                    subDays(new Date(job.created_at), 0),
+                    new Date(),
+                    { addSuffix: true }
+                  )}
+                  {" - Est. time: "} {job.deadline} days
+                </Typography>
+
+                <Typography variant="subtitle2" className={globalClasses.mb_10}>
+                  Bids: {job?.no_of_bids}
+                </Typography>
+
+                <Typography variant="subtitle2" className={globalClasses.mb_10}>
+                  <LocationOn />
+                  {job.location}
+                </Typography>
+
+                <Typography variant="h6" className={globalClasses.mb_5}>
+                  Details
+                </Typography>
+                <Typography variant="body1" className={globalClasses.mb_10}>
+                  {job.details}
+                </Typography>
+
+                <Grid
+                  container
+                  direction="row"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <Grid item>
+                    <Typography variant="h6" className={globalClasses.mb_5}>
+                      Skills Required
+                    </Typography>
+                    <Typography variant="body1" className={globalClasses.mb_20}>
+                      {job?.categories?.map((category) => (
+                        <span className={classes.categoryName}>
+                          {category.name}
+                        </span>
+                      ))}
+                    </Typography>
+                  </Grid>
+
+                  <Grid item>
+                    <Typography variant="h6" className={globalClasses.mb_5}>
+                      Posted By
+                    </Typography>
+
+                    <Grid container spacing={2}>
+                      <Grid item>
+                        <Avatar
+                          style={{ height: 65, width: 65 }}
+                          src={
+                            job.posted_by?.profile_image?.image_thumbnail_url
+                          }
+                          alt={job.posted_by?.name}
+                        />
+                      </Grid>
+                      <Grid item>
+                        <Typography variant="body1">
+                          {job.posted_by.name} <br />
+                          {job.posted_by.phone_number && (
+                            <>
+                              Ph. No: +{job?.posted_by?.phone_number} <br />
+                            </>
+                          )}
+                          <Rating
+                            value={job?.buyer_profile.rating}
+                            precision={0.5}
+                            disabled={true}
+                          />
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Card>
+            </Grid>
+          </>
         )}
         {openBidDialog && (
           <CreateBid
