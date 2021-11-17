fcc8e315173ffbc8ab3ce1c21efd0a04fbaed17a
Muhammad Talha <talhashabbir9@gmail.com>
2021-11-17 19:43:54 +0500

    validation related changes. And added images upload

--- src/logged_in/components/buyingZone/createJob/CreateJob.js
+++ src/logged_in/components/buyingZone/createJob/CreateJob.js
@@ -10,15 +10,14 @@ import {
   InputAdornment
 } from "@material-ui/core";
 
-import Checkbox from "@material-ui/icons/CheckBox";
-
-import CheckBoxOutlineBlankIcon from "@material-ui/icons/CheckBoxOutlineBlank";
-import CheckBoxIcon from "@material-ui/icons/CheckBox";
 import { Autocomplete } from "@material-ui/lab";
 
-import ButtonCircularProgress from "../../../../shared/components/ButtonCircularProgress";
 import useSaveJob from "../../../../hooks/jobs/useSaveJob";
 import useJobCategories from "../../../../hooks/jobs/useJobCategories";
+import BoxCircularProgress from "../../../../shared/components/BoxCircularProgress";
+import SnackAlert from "../../../../shared/components/SnackAlert";
+import alertSeverity from "../../../../config/alertSeverity";
+import ReactImageUploadComponent from "react-images-upload";
 
 const styles = (theme) => ({
   card: {
@@ -27,12 +26,10 @@ const styles = (theme) => ({
   }
 });
 
-const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
-const checkedIcon = <CheckBoxIcon fontSize="small" />;
-
 function CreateJob(props) {
   //const { classes } = props;
   const job = props.location?.state?.job;
+  const formRef = useRef();
   const title = useRef();
   const details = useRef();
   const budget = useRef();
@@ -41,176 +38,245 @@ function CreateJob(props) {
   const [selectedCategories, setSelectedCategories] = useState(
     job?.categories.map((category) => category.id).toString()
   );
-  const { mutate, isSuccess, isError } = useSaveJob();
+
+  // Used it to reload the form. Every react component reloads
+  // when its key changes
+  const [formKey, setFormKey] = useState("form-key");
+
+  const {
+    mutate,
+    isSuccess: isJobSaved,
+    isError: isJobSaveFailed
+  } = useSaveJob();
+
   const { data: jobCategories, isLoading } = useJobCategories();
+  const [snackMessage, setSnackMessage] = useState("");
+  const [snackOpen, setSnackOpen] = useState(false);
+  const [snackSeverity, setSnackSeverity] = useState();
+  const [images, setImages] = useState();
+  let defaultImages = [];
+
+  if (job?.id) {
+    job?.images.forEach((image) => {
+      defaultImages.push(image.image_url);
+    });
+  }
 
   const onSubmit = (e) => {
     e.preventDefault();
+    if (formRef.current.reportValidity()) {
+      const newJob = new FormData();
+      newJob.append("title", title.current.value);
+      newJob.append("details", details.current.value);
+      newJob.append("budget", budget.current.value);
+      newJob.append("deadline", deadline.current.value);
+      newJob.append("location", location.current.value);
+      newJob.append("categories", selectedCategories);
 
-    const newJob = {
-      title: title.current.value,
-      details: details.current.value,
-      budget: budget.current.value,
-      deadline: deadline.current.value,
-      location: location.current.value,
-      categories: selectedCategories,
-      job_id: job?.id
-    };
+      if (job?.id) {
+        newJob.append("job_id", job.id);
+      }
 
-    mutate(newJob);
+      if (images) {
+        let counter = 0;
+        images.forEach((image) => {
+          newJob.append(`image:${counter}`, image);
+          counter++;
+        });
+        newJob.append("no_of_images", images.length);
+      }
+
+      mutate(newJob);
+    }
   };
+
+  const clearForm = () => {
+    // Just need to change, value doesn't matter. It will reload the form
+    setFormKey(formKey + "x");
+  };
+
+  useEffect(() => {
+    if (isJobSaved) {
+      setSnackMessage("Job saved successfully!");
+      setSnackSeverity(alertSeverity.success);
+      setSnackOpen(true);
+      clearForm();
+    } else if (isJobSaveFailed) {
+      setSnackMessage(
+        "An error occured while saving the job! Please try again!"
+      );
+      setSnackSeverity(alertSeverity.error);
+      setSnackOpen(true);
+    }
+  }, [isJobSaveFailed, isJobSaved]);
 
   return (
     <Box display="flex" justifyContent="center">
       {isLoading ? (
-        <div>loading...</div>
+        <BoxCircularProgress />
       ) : (
         <Grid container spacing={3} justifyContent="center" alignItems="center">
+          {snackOpen && (
+            <SnackAlert
+              message={snackMessage}
+              severity={snackSeverity}
+              handleClose={() => {
+                setSnackOpen(false);
+              }}
+            />
+          )}
           <Grid item xs={9}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              label="Title"
-              autoFocus
-              autoComplete="off"
-              type="text"
-              FormHelperTextProps={{ error: true }}
-              inputRef={title}
-              defaultValue={job?.title}
-            />
+            <form ref={formRef} key={formKey}>
+              <ReactImageUploadComponent
+                withIcon={true}
+                withPreview={true}
+                buttonText="Choose images"
+                onChange={(pictureFiles, pictureDataURLs) => {
+                  setImages(pictureFiles);
+                }}
+                imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+                maxFileSize={5242880} // 5mb
+                //defaultImages={defaultImages} // TODO Unable to change these images
+              />
 
-            <TextField
-              variant="outlined"
-              margin="normal"
-              multiline
-              maxRows={5}
-              required
-              fullWidth
-              label="Details"
-              autoComplete="off"
-              type="text"
-              FormHelperTextProps={{ error: true }}
-              inputRef={details}
-              defaultValue={job?.details}
-            />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="Title"
+                autoFocus
+                autoComplete="off"
+                type="text"
+                FormHelperTextProps={{ error: true }}
+                inputRef={title}
+                defaultValue={job?.title}
+              />
 
-            <Autocomplete
-              multiple
-              options={jobCategories}
-              disableCloseOnSelect
-              getOptionLabel={(option) => option.name}
-              filterSelectedOptions
-              renderOption={(option, props) => {
-                return (
-                  <li {...props}>
-                    {/* <Checkbox
-                      //icon={<CheckBoxIcon fontSize="small" />}
-                      //checkedIcon={<CheckBoxIcon fontSize="small" />}
-                      style={{ marginRight: 8 }}
-                      checked={props.selected}
-                    /> */}
-                    {option.name}
-                  </li>
-                );
-              }}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  label="Categories"
-                  placeholder="Categories"
-                  variant="outlined"
-                />
-              )}
-              onChange={(event, newValue) => {
-                // Get only ids of selected categories
-                setSelectedCategories(
-                  newValue.map((category) => category.id).toString()
-                );
-              }}
-              defaultValue={job?.categories}
-            />
+              <Autocomplete
+                multiple
+                required
+                options={jobCategories}
+                disableCloseOnSelect
+                getOptionLabel={(option) => option.name}
+                filterSelectedOptions
+                renderOption={(option, props) => {
+                  return <li {...props}>{option.name}</li>;
+                }}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Categories"
+                    placeholder="Categories"
+                    variant="outlined"
+                  />
+                )}
+                onChange={(event, newValue) => {
+                  // Get only ids of selected categories
+                  setSelectedCategories(
+                    newValue.map((category) => category.id).toString()
+                  );
+                }}
+                defaultValue={job?.categories}
+              />
 
-            <Grid
-              container
-              direction="row"
-              spacing={3}
-              justifyContent="space-between"
-              alignItems="center"
-            >
-              <Grid item>
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  style={{
-                    width: "20ch"
-                  }}
-                  required
-                  label="Budget"
-                  autoComplete="off"
-                  type="number"
-                  FormHelperTextProps={{ error: true }}
-                  inputRef={budget}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">RS</InputAdornment>
-                    )
-                  }}
-                  defaultValue={job?.budget}
-                />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                multiline
+                rows={4}
+                maxRows={5}
+                required
+                fullWidth
+                label="Details"
+                placeholder="Describe in detail about the job and your expectations from the worker"
+                autoComplete="off"
+                type="text"
+                FormHelperTextProps={{ error: true }}
+                inputRef={details}
+                defaultValue={job?.details}
+              />
+
+              <Grid
+                container
+                direction="row"
+                spacing={3}
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                <Grid item>
+                  <TextField
+                    variant="outlined"
+                    margin="normal"
+                    style={{
+                      width: "20ch"
+                    }}
+                    required
+                    label="Budget"
+                    autoComplete="off"
+                    type="number"
+                    FormHelperTextProps={{ error: true }}
+                    inputRef={budget}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">RS</InputAdornment>
+                      )
+                    }}
+                    defaultValue={job?.budget}
+                  />
+                </Grid>
+                <Grid item>
+                  <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    label="Location"
+                    autoComplete="off"
+                    type="text"
+                    FormHelperTextProps={{ error: true }}
+                    inputRef={location}
+                    defaultValue={job?.location}
+                  />
+                </Grid>
+
+                <Grid item>
+                  <TextField
+                    variant="outlined"
+                    margin="normal"
+                    width="auto"
+                    required
+                    style={{
+                      width: "20ch"
+                    }}
+                    label="Deadline"
+                    autoComplete="off"
+                    type="number"
+                    FormHelperTextProps={{ error: true }}
+                    inputRef={deadline}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">days</InputAdornment>
+                      )
+                    }}
+                    defaultValue={job?.deadline}
+                  />
+                </Grid>
               </Grid>
-              <Grid item>
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  label="Location"
-                  autoComplete="off"
-                  type="text"
-                  FormHelperTextProps={{ error: true }}
-                  inputRef={location}
-                  defaultValue={job?.location}
-                />
-              </Grid>
-
-              <Grid item>
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  width="auto"
-                  required
-                  style={{
-                    width: "20ch"
-                  }}
-                  label="Deadline"
-                  autoComplete="off"
-                  type="number"
-                  FormHelperTextProps={{ error: true }}
-                  inputRef={deadline}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">days</InputAdornment>
-                    )
-                  }}
-                  defaultValue={job?.deadline}
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="secondary"
-              // disabled={isLoading}
-              size="large"
-              onClick={onSubmit}
-              style={{
-                marginTop: 20
-              }}
-            >
-              Save
-            </Button>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="secondary"
+                // disabled={isLoading}
+                size="large"
+                onClick={onSubmit}
+                style={{
+                  marginTop: 20
+                }}
+              >
+                Save
+              </Button>
+            </form>
           </Grid>
         </Grid>
       )}
