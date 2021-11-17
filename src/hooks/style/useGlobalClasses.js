1d1f4b926f019ca4bd682410bd8c6b1e9b6b0635
Muhammad Talha <talhashabbir9@gmail.com>
2021-11-17 19:36:10 +0500

    created hook for global css styles

---
+++ src/hooks/style/useGlobalClasses.js
@@ -0,0 +1,15 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useGlobalClasses = makeStyles((theme) => ({
+  mb_10: {
+    marginBottom: "10px"
+  },
+  mb_20: {
+    marginBottom: "20px"
+  },
+  mb_5: {
+    marginBottom: "5px"
+  }
+}));
+
+export default useGlobalClasses;
