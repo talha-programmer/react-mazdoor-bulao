a7e1a823c13e923532aa5aff49fb88bea6917882
Muhammad Talha <talhashabbir9@gmail.com>
2021-11-17 19:34:22 +0500

    added alert severity options

---
+++ src/config/alertSeverity.js
@@ -0,0 +1,11 @@
+function alertSeverity() {
+  // severity options for the SnackAlert Component
+  return {
+    error: "error",
+    warning: "warning",
+    info: "info",
+    success: "success"
+  };
+}
+
+export default alertSeverity();
