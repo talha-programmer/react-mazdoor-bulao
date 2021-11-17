e3faa6791b34b7bc67fa718d59e676506ca89d36
Muhammad Talha <talhashabbir9@gmail.com>
2021-11-17 19:36:53 +0500

    don't get applied jobs if user is not logged in

--- src/hooks/user/useAppliedJobs.js
+++ src/hooks/user/useAppliedJobs.js
@@ -2,11 +2,17 @@ import axios from "axios";
 import { useQuery } from "react-query";
 import api from "../../config/api";
 import queryKeys from "../../config/queryKeys";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 function useAppliedJobs() {
+  const { token } = useContext(AuthContext);
   async function getUser() {
     let appliedJobs = null;
     try {
+      if (!token) {
+        return null;
+      }
       await axios
         .post(api.appliedJobs)
         .then((result) => (appliedJobs = result.data.applied_jobs));
